135abe4448ae2ca64a292cbf86054629a1f28dbb
Rob Gonsalves <robgon-art@gmail.com>
2025-04-03 09:44:11 -0400

    Fix warnings when running tests

--- src/Clip/ClipView.test.tsx
+++ src/Clip/ClipView.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import ClipView from './ClipView';
 import { ClipViewProps } from './ClipViewModel';
 
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,28 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Set up the test environment to suppress warnings about ReactDOMTestUtils.act
+import { configure } from '@testing-library/react';
+
+configure({
+  testIdAttribute: 'data-testid',
+});
+
+// Suppress the act() warnings
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning: ReactDOM.render is no longer supported in React 18./.test(args[0])) {
+      return;
+    }
+    if (/Warning:.*react-dom-test-utils/.test(args[0])) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
+});
