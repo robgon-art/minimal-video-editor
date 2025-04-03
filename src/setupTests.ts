fbf72c5c4a05939b4b27b60ba2f27e0f1c4e3e20
Rob Gonsalves <robgon-art@gmail.com>
2025-04-03 09:48:31 -0400

    Removed warning suppression code.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,27 +4,9 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
-// Set up the test environment to suppress warnings about ReactDOMTestUtils.act
+// Basic test configuration
 import { configure } from '@testing-library/react';
 
 configure({
   testIdAttribute: 'data-testid',
-});
-
-// Suppress the act() warnings
-const originalError = console.error;
-beforeAll(() => {
-  console.error = (...args) => {
-    if (/Warning: ReactDOM.render is no longer supported in React 18./.test(args[0])) {
-      return;
-    }
-    if (/Warning:.*react-dom-test-utils/.test(args[0])) {
-      return;
-    }
-    originalError.call(console, ...args);
-  };
-});
-
-afterAll(() => {
-  console.error = originalError;
 });
